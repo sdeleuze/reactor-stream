a5fa455ccdcf8113edba21609f90c90698d6f4f6
Stephane Maldini <smaldini@pivotal.io>
2016-02-02 09:48:42 +0000

    fix random test failure ?

--- src/test/java/reactor/rx/StreamCombinationTests.java
+++ src/test/java/reactor/rx/StreamCombinationTests.java
@@ -155,7 +155,8 @@ public class StreamCombinationTests extends AbstractReactorTest {
 		int elements = 40;
 		CountDownLatch latch = new CountDownLatch(elements / 2);
 
-		Control tail = Stream.combineLatest(sensorOdd().cache().throttleRequest(50), sensorEven().cache().throttleRequest(50),
+		Control tail = Stream.combineLatest(sensorOdd().cache().throttleRequest(100), sensorEven().cache()
+		                                                                                         .throttleRequest(100),
 		this::computeMin)
 		                      .log("combineLatest")
 		                      .consume(i -> latch.countDown(), null, latch::countDown);
