d0161deb65dde0da3406e5b400a75d78c3d78cf6
Stephane Maldini <smaldini@pivotal.io>
2016-02-26 15:57:36 +0000

    javadoc

--- src/main/java/reactor/rx/Broadcaster.java
+++ src/main/java/reactor/rx/Broadcaster.java
@@ -42,18 +42,20 @@ import reactor.rx.subscriber.SerializedSubscriber;
 
 /**
  * {@link Broadcaster} is an identity {@link Processor} extending {@link Stream} qualified for "Hot" sequence
- * generation. The message passing strategy can be
- * chosen
- * over the various factories including {@link #async async}, {@link #replay replaying} or {@link #blocking blocking}.
+ * generation. The message passing strategy can be chosen over the various factories including
+ * {@link #async async}, {@link #replay replaying} or {@link #blocking blocking}.
  * <p>
- * A {@link Broadcaster is similar to Reactive Extensions Subjects. Some broadcasters might be shared and will require
- * {@link #serialize serialization} as onXXXX handle should not be invoke concurrently. It is recommended
+ * A {@link Broadcaster} is similar to Reactive Extensions Subjects. Some broadcasters might be shared and will require
+ * {@link #serialize serialization} as onXXXX handle should not be invoke concurrently. It is recommended to use the
+ * safe {@link #startEmitter()} gateway to safely {@link SignalEmitter#emit(Object) emit} without failing
+ * backpressure protocol. OnNext signals should not be invoked without demand and unpredicted behavior might occur in
+ * this case.
  *
  * <p>
  * <img width="640" src="https://raw.githubusercontent.com/reactor/projectreactor.io/master/src/main/static/assets/img/marble/broadcaster.png" alt="">
  *
  * <p>Excluding {@link #from arbitrary} unicast
- * {@link Processor}, the only unicast restricted {@link Broadcaster} are {@link #blocking()} and {@link #unicast()}.
+ * {@link Processor}, the unicast restricted {@link Broadcaster} are {@link #blocking()} and {@link #unicast()}.
  *
  * @param <O> the replayed type
  *
@@ -139,53 +141,6 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 	/**
 	 * Build a {@literal Broadcaster}, ready to broadcast values with {@link Broadcaster#onNext(Object)}, {@link
 	 * Broadcaster#onError(Throwable)}, {@link Broadcaster#onComplete()}. Values broadcasted are directly consumable by
-	 * subscribing to the returned instance. <p> Will not bubble up  any {@link Exceptions.CancelException}
-	 * @param <T> the replayed type
-	 * @return an eventually interruptable unicast {@link Broadcaster}
-	 */
-	public static <T> Broadcaster<T> unicast() {
-		return unicast(null);
-	}
-
-	/**
-	 * Build a {@literal Broadcaster}, ready to broadcast values with {@link Broadcaster#onNext(Object)}, {@link
-	 * Broadcaster#onError(Throwable)}, {@link Broadcaster#onComplete()}. Values broadcasted are directly consumable by
-	 * subscribing to the returned instance. <p> Will not bubble up  any {@link Exceptions.CancelException}
-	 * @param timer the Reactor {@link Timer} to use downstream
-	 * @param <T> the replayed type
-	 * @return an eventually interruptable unicast {@link Broadcaster}
-	 */
-	public static <T> Broadcaster<T> unicast(Timer timer) {
-		return from(new UnicastProcessor<>(QueueSupplier.<T>small(true).get()), timer, true);
-	}
-
-	/**
-	 * Build a {@literal Broadcaster} that will support concurrent signals (onNext, onError, onComplete) and use
-	 * thread-stealing to serialize underlying emitter processor calls.
-	 *
-	 * @param <T> the replayed type
-	 * @return a serializing unicast {@link Broadcaster}
-	 */
-	public static <T> Broadcaster<T> serialize() {
-		return serialize(null);
-	}
-
-	/**
-	 * Build a {@literal Broadcaster} that will support concurrent signals (onNext, onError, onComplete) and use
-	 * thread-stealing to serialize underlying emitter processor calls.
-	 *
-	 * @param timer the Reactor {@link Timer} to use downstream
-	 * @param <T> the replayed type
-	 * @return a serializing unicast {@link Broadcaster}
-	 */
-	public static <T> Broadcaster<T> serialize(Timer timer) {
-		Processor<T, T> processor = EmitterProcessor.create();
-		return new Broadcaster<T>(SerializedSubscriber.create(processor), processor, timer, true);
-	}
-
-	/**
-	 * Build a {@literal Broadcaster}, ready to broadcast values with {@link Broadcaster#onNext(Object)}, {@link
-	 * Broadcaster#onError(Throwable)}, {@link Broadcaster#onComplete()}. Values broadcasted are directly consumable by
 	 * subscribing to the returned instance.
 	 * @param emitter Identity processor to support broadcasting
 	 * @param <T> the replayed type
@@ -246,6 +201,20 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 	}
 
 	/**
+	 * Build a {@literal Broadcaster}, first broadcasting the most recent signal then ready to broadcast values with
+	 * {@link #onNext(Object)},
+	 * {@link Broadcaster#onError(Throwable)}, {@link Broadcaster#onComplete()}.
+	 * Values broadcasted are directly consumable by subscribing to the returned instance.
+	 *
+	 * @param timer the {@link Timer} to use downstream
+	 * @param <T>  the replayed type
+	 * @return a non interruptable last item replaying pub-sub {@link Broadcaster}
+	 */
+	public static <T> Broadcaster<T> replayLast(Timer timer) {
+		return replayLastOrDefault(null, timer);
+	}
+
+	/**
 	 * Build a {@literal Broadcaster}, rfirst broadcasting the most recent signal then starting with the passed value,
 	 * then ready to broadcast values with {@link reactor.rx
 	 * .Broadcaster#onNext(Object)},
@@ -263,20 +232,6 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 	 */
 	public static <T> Broadcaster<T> replayLastOrDefault(T value) {
 		return replayLastOrDefault(value, null);
-	}
-
-	/**
-	 * Build a {@literal Broadcaster}, first broadcasting the most recent signal then ready to broadcast values with
-	 * {@link #onNext(Object)},
-	 * {@link Broadcaster#onError(Throwable)}, {@link Broadcaster#onComplete()}.
-	 * Values broadcasted are directly consumable by subscribing to the returned instance.
-	 *
-	 * @param timer the {@link Timer} to use downstream
-	 * @param <T>  the replayed type
-	 * @return a non interruptable last item replaying pub-sub {@link Broadcaster}
-	 */
-	public static <T> Broadcaster<T> replayLast(Timer timer) {
-		return replayLastOrDefault(null, timer);
 	}
 
 	/**
@@ -299,24 +254,51 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 	}
 
 	/**
-	 * Create a {@link SignalEmitter} and attach it via {@link #onSubscribe(Subscription)}.
+	 * Build a {@literal Broadcaster} that will support concurrent signals (onNext, onError, onComplete) and use
+	 * thread-stealing to serialize underlying emitter processor calls.
 	 *
-	 * @return a new subscribed {@link SignalEmitter}
+	 * @param <T> the replayed type
+	 * @return a serializing unicast {@link Broadcaster}
 	 */
-	public SignalEmitter<O> startEmitter() {
-		return bindEmitter(true);
+	public static <T> Broadcaster<T> serialize() {
+		return serialize(null);
 	}
 
 	/**
-	 * Prepare a {@link SignalEmitter} and pass it to {@link #onSubscribe(Subscription)} if the autostart flag is
-	 * set to true.
+	 * Build a {@literal Broadcaster} that will support concurrent signals (onNext, onError, onComplete) and use
+	 * thread-stealing to serialize underlying emitter processor calls.
 	 *
-	 * @return a new {@link SignalEmitter}
+	 * @param timer the Reactor {@link Timer} to use downstream
+	 * @param <T> the replayed type
+	 * @return a serializing unicast {@link Broadcaster}
 	 */
-	public SignalEmitter<O> bindEmitter(boolean autostart) {
-		return SignalEmitter.create(this, autostart);
+	public static <T> Broadcaster<T> serialize(Timer timer) {
+		Processor<T, T> processor = EmitterProcessor.create();
+		return new Broadcaster<T>(SerializedSubscriber.create(processor), processor, timer, true);
 	}
 
+	/**
+	 * Build a {@literal Broadcaster}, ready to broadcast values with {@link Broadcaster#onNext(Object)}, {@link
+	 * Broadcaster#onError(Throwable)}, {@link Broadcaster#onComplete()}. Values broadcasted are directly consumable by
+	 * subscribing to the returned instance. <p> Will not bubble up  any {@link Exceptions.CancelException}
+	 * @param <T> the replayed type
+	 * @return an eventually interruptable unicast {@link Broadcaster}
+	 */
+	public static <T> Broadcaster<T> unicast() {
+		return unicast(null);
+	}
+
+	/**
+	 * Build a {@literal Broadcaster}, ready to broadcast values with {@link Broadcaster#onNext(Object)}, {@link
+	 * Broadcaster#onError(Throwable)}, {@link Broadcaster#onComplete()}. Values broadcasted are directly consumable by
+	 * subscribing to the returned instance. <p> Will not bubble up  any {@link Exceptions.CancelException}
+	 * @param timer the Reactor {@link Timer} to use downstream
+	 * @param <T> the replayed type
+	 * @return an eventually interruptable unicast {@link Broadcaster}
+	 */
+	public static <T> Broadcaster<T> unicast(Timer timer) {
+		return from(new UnicastProcessor<>(QueueSupplier.<T>small(true).get()), timer, true);
+	}
 	/**
 	 * INTERNAL
 	 */
@@ -324,11 +306,9 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 	final Timer            timer;
 	final boolean          ignoreDropped;
 	final SwapSubscription subscription;
-
 	protected Broadcaster(Processor<O, O> processor, Timer timer, boolean ignoreDropped) {
 		this(processor, processor, timer, ignoreDropped);
 	}
-
 	protected Broadcaster(
 			Subscriber<O> receiver,
 			Publisher<O> publisher,
@@ -342,9 +322,39 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 		receiver.onSubscribe(subscription);
 	}
 
+	/**
+	 * Prepare a {@link SignalEmitter} and pass it to {@link #onSubscribe(Subscription)} if the autostart flag is
+	 * set to true.
+	 *
+	 * @return a new {@link SignalEmitter}
+	 */
+	public SignalEmitter<O> bindEmitter(boolean autostart) {
+		return SignalEmitter.create(this, autostart);
+	}
+
 	@Override
-	public void onSubscribe(Subscription subscription) {
-		this.subscription.swapTo(subscription);
+	public Timer getTimer() {
+		return timer != null ? timer : Timer.globalOrNull();
+	}
+
+	@Override
+	public void onComplete() {
+		try {
+			receiver.onComplete();
+		}
+		catch (Exceptions.InsufficientCapacityException | Exceptions.CancelException c) {
+			//IGNORE
+		}
+	}
+
+	@Override
+	public void onError(Throwable t) {
+		try {
+			receiver.onError(t);
+		}
+		catch (Exceptions.InsufficientCapacityException | Exceptions.CancelException c) {
+			//IGNORE
+		}
 	}
 
 	@Override
@@ -364,73 +374,36 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 	}
 
 	@Override
-	public void onError(Throwable t) {
-		try {
-			receiver.onError(t);
-		}
-		catch (Exceptions.InsufficientCapacityException | Exceptions.CancelException c) {
-			//IGNORE
-		}
+	public void onSubscribe(Subscription subscription) {
+		this.subscription.swapTo(subscription);
 	}
 
-	@Override
-	public void onComplete() {
-		try {
-			receiver.onComplete();
-		}
-		catch (Exceptions.InsufficientCapacityException | Exceptions.CancelException c) {
-			//IGNORE
-		}
-	}
-
-	@Override
-	public Timer getTimer() {
-		return timer != null ? timer : Timer.globalOrNull();
+	/**
+	 * Create a {@link SignalEmitter} and attach it via {@link #onSubscribe(Subscription)}.
+	 *
+	 * @return a new subscribed {@link SignalEmitter}
+	 */
+	public SignalEmitter<O> startEmitter() {
+		return bindEmitter(true);
 	}
 
 	static final class SwapSubscription implements Subscription, Receiver, Completable, Introspectable {
 
-		@SuppressWarnings("unused")
-		volatile Subscription subscription;
-		static final AtomicReferenceFieldUpdater<SwapSubscription, Subscription> SUBSCRIPTION =
-				PlatformDependent.newAtomicReferenceFieldUpdater(SwapSubscription.class, "subscription");
-
-
-		@SuppressWarnings("unused")
-		volatile long requested;
 		protected static final AtomicLongFieldUpdater<SwapSubscription> REQUESTED =
 				AtomicLongFieldUpdater.newUpdater(SwapSubscription.class, "requested");
+		static final AtomicReferenceFieldUpdater<SwapSubscription, Subscription> SUBSCRIPTION =
+				PlatformDependent.newAtomicReferenceFieldUpdater(SwapSubscription.class, "subscription");
 
 		public static SwapSubscription create() {
 			return new SwapSubscription();
 		}
+		@SuppressWarnings("unused")
+		volatile Subscription subscription;
+		@SuppressWarnings("unused")
+		volatile long requested;
 
 		SwapSubscription() {
 			SUBSCRIPTION.lazySet(this, EmptySubscription.INSTANCE);
-		}
-
-		/**
-		 *
-		 * @param subscription
-		 */
-		public void swapTo(Subscription subscription) {
-			Subscription old = SUBSCRIPTION.getAndSet(this, subscription);
-			if(old != EmptySubscription.INSTANCE){
-				subscription.cancel();
-				return;
-			}
-			long r = REQUESTED.getAndSet(this, 0L);
-			if(r != 0L){
-				subscription.request(r);
-			}
-		}
-
-		/**
-		 *
-		 * @return
-		 */
-		public boolean isUnsubscribed(){
-			return subscription == EmptySubscription.INSTANCE;
 		}
 
 		/**
@@ -450,21 +423,6 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 			return BackpressureUtils.getAndSub(REQUESTED, this, 1L) != 0;
 		}
 
-		/**
-		 *
-		 * @return
-		 */
-		public boolean isCancelled(){
-			return subscription == CancelledSubscription.INSTANCE;
-		}
-
-		@Override
-		public void request(long n) {
-			BackpressureUtils.getAndAdd(REQUESTED, this, n);
-			SUBSCRIPTION.get(this)
-			            .request(n);
-		}
-
 		@Override
 		public void cancel() {
 			Subscription s;
@@ -482,8 +440,21 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 		}
 
 		@Override
-		public Object upstream() {
-			return subscription;
+		public int getMode() {
+			return 0;
+		}
+
+		@Override
+		public String getName() {
+			return null;
+		}
+
+		/**
+		 *
+		 * @return
+		 */
+		public boolean isCancelled(){
+			return subscription == CancelledSubscription.INSTANCE;
 		}
 
 		@Override
@@ -496,14 +467,35 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 			return isUnsubscribed();
 		}
 
-		@Override
-		public int getMode() {
-			return 0;
+		/**
+		 *
+		 * @return
+		 */
+		public boolean isUnsubscribed(){
+			return subscription == EmptySubscription.INSTANCE;
 		}
 
 		@Override
-		public String getName() {
-			return null;
+		public void request(long n) {
+			BackpressureUtils.getAndAdd(REQUESTED, this, n);
+			SUBSCRIPTION.get(this)
+			            .request(n);
+		}
+
+		/**
+		 *
+		 * @param subscription
+		 */
+		public void swapTo(Subscription subscription) {
+			Subscription old = SUBSCRIPTION.getAndSet(this, subscription);
+			if(old != EmptySubscription.INSTANCE){
+				subscription.cancel();
+				return;
+			}
+			long r = REQUESTED.getAndSet(this, 0L);
+			if(r != 0L){
+				subscription.request(r);
+			}
 		}
 
 		@Override
@@ -512,6 +504,11 @@ public class Broadcaster<O> extends StreamProcessor<O, O> {
 					"subscription=" + subscription +
 					", requested=" + requested +
 					'}';
+		}
+
+		@Override
+		public Object upstream() {
+			return subscription;
 		}
 	}
 }
